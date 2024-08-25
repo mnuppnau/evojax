b6b1d27c9bb9c84a442ad0f2da01a1e2058da90b
mnuppnau <hi6094@wayne.edu>
2024-08-25 10:01:35 -0400

    started the infogan training example, and updated the trainer code to include additional tasks, policies, and solvers

--- evojax/policy/convnet.py
+++ evojax/policy/convnet.py
@@ -43,8 +43,62 @@ class CNN(nn.Module):
         x = nn.log_softmax(x)
         return x
 
+def generate_latent_points(rng, latent_dim, n_samples):
+      rng, latent_rng, cat_rng = jrandom.split(rng, num=3)
+      latent_inputs = jrandom.normal(latent_rng, (n_samples, latent_dim))
+      cat_codes = jrandom.randint(cat_rng, (n_samples,), 0, 10)
+      cat_codes = nn.one_hot(cat_codes, 10) z_input = jnp.concatenate((latent_inputs, cat_codes), axis=1)
+    
+      return z_input, cat_codes
 
-class ConvNetPolicy(PolicyNetwork):
+class Generator(nn.Module):
+    """ Generator CNN for MNIST """
+
+    @nn.compact
+    def __call__(self, z):
+        z = z.reshape((z.shape[0], 1, 1, z.shape[1]))
+        x = nn.ConvTranspose(self.features*4, [3, 3], [2, 2], 'VALID', kernel_init=normal_init(0.02))(z)
+        x = nn.BatchNorm(not self.training, -1, 0.1, scale_init=normal_init(0.02))(x)
+        x = nn.relu(x)
+        x = nn.ConvTranspose(self.features*2, [4, 4], [1, 1], 'VALID', kernel_init=normal_init(0.02))(x)
+        x = nn.BatchNorm(not self.training, -1, 0.1, scale_init=normal_init(0.02))(x)
+        x = nn.relu(x)
+        x = nn.ConvTranspose(self.features, [3, 3], [2, 2], 'VALID', kernel_init=normal_init(0.02))(x)
+        x = nn.BatchNorm(not self.training, -1, 0.1, scale_init=normal_init(0.02))(x)
+        x = nn.relu(x)
+        x = nn.ConvTranspose(1, [4, 4], [2, 2], 'VALID', kernel_init=normal_init(0.02))(x)
+        x = jnp.tanh(x)
+        return x
+
+class Discriminator(nn.Module):
+    features: int = 64
+    training: bool = True
+
+    q_cat: int = 10
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(self.features, [4, 4], [2, 2], 'VALID', kernel_init=normal_init(0.02))(x)
+        x = nn.BatchNorm(not self.training, -1, 0.1, scale_init=normal_init(0.02))(x)
+        x = nn.leaky_relu(x, 0.2)
+        x = nn.Conv(self.features*2, [4, 4], [2, 2], 'VALID', kernel_init=normal_init(0.02))(x)
+        x = nn.BatchNorm(not self.training, -1, 0.1, scale_init=normal_init(0.02))(x)
+        x = nn.leaky_relu(x, 0.2)
+        
+        # Discriminator output
+        d = nn.Conv(1, [4, 4], [2, 2], 'VALID', kernel_init=normal_init(0.02))(x)
+        d = d.reshape((d.shape[0], -1))
+
+        # Q outpiut
+        q = nn.Conv(self.features*2, [4, 4], [2, 2], 'VALID', kernel_init=normal_init(0.02))(x)
+        q = nn.BatchNorm(not self.training, -1, 0.1, scale_init=normal_init(0.02))(q)
+        q = nn.leaky_relu(q, 0.2)
+        q = nn.Conv(self.q_cat, [1, 1], [2, 2], 'VALID', kernel_init=normal_init(0.02))(q)
+        q = q.reshape((q.shape[0], -1))
+        return d, q
+
+
+class GenPolicy(PolicyNetwork):
     """A convolutional neural network for the MNIST classification task."""
 
     def __init__(self, logger: logging.Logger = None):
@@ -53,13 +107,50 @@ class ConvNetPolicy(PolicyNetwork):
         else:
             self._logger = logger
 
-        model = CNN()
-        params = model.init(random.PRNGKey(0), jnp.zeros([1, 28, 28, 1]))
+        self.model_gen = Generator()
+        self.model_disc = Discriminator()
+        
+        key = random.PRNGKey(0)
+
+        key, key_gen, key_disc, key_latent = random.split(key, 4)
+
+        noise = random.normal(key_latent, (100, 64))
+        c = jnp.tile(jnp.arrange(10), 10)
+        c = jax.nn.one_hot(c, 10)
+
+        latent = jnp.concatenate([noise, c], axis=-1)
+
+        image_shape = (1, 28, 28, 1)
+
+        variables_gen = self.model_gen.init(key_gen, jnp.ones(latent.shape, jnp.float32))
+        variables_disc = self.model_disc.init(key_disc, jnp.ones(image_shape, jnp.float32))
+
+        self.init_params_gen, self.init_batch_stats_gen = variables_gen['params'], variables_gen['batch_stats']
+        self.init_params_disc, self.init_batch_stats_disc = variables_disc['params'], variables_disc['batch_stats']
+
+        self.latent_dim = 64
+        self.batch_size = 512
+  
         self.num_params, format_params_fn = get_params_format_fn(params)
         self._logger.info(
             'ConvNetPolicy.num_params = {}'.format(self.num_params))
         self._format_params_fn = jax.vmap(format_params_fn)
-        self._forward_fn = jax.vmap(model.apply)
+
+    def forward_fn_gen(self, params_g, vars_g_batch_stats, params_d, vars_d_batch_stats):
+          
+        z_input, cat_one_hot = generate_latent_points(key, self.latent_dim, self.batch_size)
+
+        fake_data, vars_g = self.model_gen.apply({'params': params_g, 'batch_stats': vars_g_batch_stats}, z_input, mutable=['batch_stats'])
+        (preds, q), vars_d = self.model_disc.apply({'params': params_d, 'batch_stats': vars_d_batch_stats}, fake_data, mutable=['batch_stats'])
+
+        return fake_data, preds, q, vars_g, vars_d, q
+
+    def forward_fn_disc(self, params_d, vars_d_batch_stats, real_data, fake_data):
+        
+        (real_preds, _), vars_d = self.model_disc.apply({'params': params_d, 'batch_stats': vars_d_batch_stats}, real_data, mutable=['batch_stats'])
+        (fake_preds, q), vars_d = self.model_disc.apply({'params': params_d, 'batch_stats': vars_d_batch_stats}, fake_data, mutable=['batch_stats'])
+
+        return real_preds, fake_preds, q, vars_d
 
     def get_actions(self,
                     t_states: TaskState,
